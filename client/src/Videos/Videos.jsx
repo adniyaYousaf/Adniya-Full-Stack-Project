3bfda6a5d6875f5cf613ebf66838e7e958dd39a3
= <adniyayousaf92@gmail.com>
2024-07-11 22:01:17 +0100

    added some styles

--- client/src/Videos/Videos.jsx
+++ client/src/Videos/Videos.jsx
@@ -3,62 +3,62 @@ import { FaHeart } from "react-icons/fa";
 import "./videos.scss";
 
 const formatDate = (d) => {
-	let date = new Date(d);
-	return date.toDateString();
+  let date = new Date(d);
+  return date.toDateString();
 };
 
 const Videos = (props) => {
-	const mapVideos = props.video.map((video, index) => {
-		return (
-			<div key={index} className="container">
-				<iframe
-					src={video.src}
-					title="Video recommendation"
-					className="container_video"
-				></iframe>
-				<div className="container_btns">
-					<div className="container_btns_time">
-						<p>
-							<strong>Time:</strong><span> {formatDate(video.addeddate)}</span>
-						</p>
-					</div>
-					<div className="container_btns_thumb-box">
-						Up Vote<FaThumbsUp
-							className="container_btns_thumb-box_thumb"
-							onClick={() => props.update(video.id, "up")}
-						/>
-					</div>
-					<div className="container_btns_thumb-box">
-						Down Vote<FaThumbsDown
-							className="container_btns_thumb-box_thumb"
-							onClick={() => props.update(video.id, "down")}
-						/>
-					</div>
-					<button
-						className="container_btns_delbtn"
-						onClick={() => props.click(video.id)}
-					>
-						Remove video
-					</button>
-				</div>
-				<div className="container_title">
-					<p >{video.title}</p>
-					<p className="container_title_rating">
-						Rating {video.rating}
-						<span>
-							<FaHeart />
-						</span>
-					</p>
-				</div>
-			</div>
-		);
-	});
+  const mapVideos = props.video.map((video, index) => {
+    return (
+      <div key={index} className="container">
+        <iframe
+          src={video.src}
+          title="Video recommendation"
+          className="container_video"
+        ></iframe>
+        <div className="container_btns">
+          <div className="container_btns_time">
+            <p>{formatDate(video.addeddate)}</p>
+          </div>
+          <div
+            className="container_btns_thumb-box"
+            onClick={() => props.update(video.id, "up")}
+          >
+            Up Vote
+            <FaThumbsUp className="container_btns_thumb-box_thumb" />
+          </div>
+          <div
+            className="container_btns_thumb-box"
+            onClick={() => props.update(video.id, "down")}
+          >
+            Down Vote
+            <FaThumbsDown className="container_btns_thumb-box_thumb" />
+          </div>
+          <button
+            className="container_btns_delbtn"
+            onClick={() => props.click(video.id)}
+          >
+            Remove video
+          </button>
+        </div>
+        <div className="container_title">
+          <p>{video.title}</p>
+          <p className="container_title_rating">
+            Rating {video.rating}
+            <span>
+              <FaHeart />
+            </span>
+          </p>
+        </div>
+      </div>
+    );
+  });
 
-	return (
-		<>
-			<div className="container-box">{mapVideos}</div>
-		</>
-	);
+  return (
+    <>
+      <div className="container-box">{mapVideos}</div>
+    </>
+  );
 };
 
 export default Videos;
